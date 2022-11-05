d95d4f3a847bafd6422f43606237f7e9445a5196
易鑫 <58894205+yixinNB@users.noreply.github.com>
2022-11-05 23:22:44 +0800

    add fingerprintjs

--- dist/fpCollect.d.ts
+++
@@ -1,7 +0,0 @@
-export namespace fpCollect {
-    export { addCustomFunction };
-    export { generateFingerprint };
-}
-declare function addCustomFunction(name: any, isAsync: any, f: any): void;
-declare function generateFingerprint(): Promise<any>;
-export {};
